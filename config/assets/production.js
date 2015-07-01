4f121545c00e8eb87b42eb75adbf57d4d023020f
Lovell Felix <lovell.felix@gmail.com>
2015-07-01 08:20:20 -0500

    Added angular material

--- config/assets/production.js
+++ config/assets/production.js
@@ -5,11 +5,14 @@ module.exports = {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
+				'public/lib/angular-material/angular-material.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
+				'public/lib/angular-aria/angular-aria.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-material/angular-material.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
