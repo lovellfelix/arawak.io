12e0a40e9795a2fafdd77bbae0d57be2f137b618
Lovell Felix <lovell.felix@gmail.com>
2015-07-01 08:20:20 -0500

    Reverted back angular-material

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,12 +5,14 @@ module.exports = {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/material-design-lite/material.min.css'
+				'public/lib/angular-material/angular-material.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
+				'public/lib/angular-aria/angular-aria.js',
 				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-animate/angular-animate.js',
+				'public/lib/angular-material/angular-material.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
