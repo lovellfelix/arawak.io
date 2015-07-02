9ad0040eb35e3fd3b008db0209139afd2c105173
Lovell Felix <lovell.felix@gmail.com>
2015-07-02 09:29:32 -0700

    Update application information

--- config/env/default.js
+++ config/env/default.js
@@ -2,13 +2,13 @@
 
 module.exports = {
 	app: {
-		title: 'MEAN.JS',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+		title: 'Arawak',
+		description: '',
+		keywords: '',
 		googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
+	sessionSecret: 'elcinawillneverbeforgotten',
 	sessionCollection: 'sessions'
 };
