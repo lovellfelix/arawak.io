2d746dcb80bdf15bf4863b94bddf18310be70726
Lovell Felix <lovell.felix@gmail.com>
2015-07-02 07:59:08 -0500

    Added redirect to dashboard when authenticated

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,11 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$http', '$location', 'Authentication',
+	function($scope, $http, $location, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		// If user is signed in then redirect back home
+		if ($scope.authentication.user) $location.path('/dashboard');
 	}
 ]);
