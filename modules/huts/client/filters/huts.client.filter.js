8ae4628a9d0c58ac10db97601cf25bfa23882a64
Lovell Felix <lovell.felix@gmail.com>
2015-07-04 08:31:46 -0500

    WIP: Added huts filter

---
+++ modules/huts/client/filters/huts.client.filter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('huts').filter('Huts', [
+	function() {
+		return function(input) {
+			// Huts directive logic
+			// ...
+			var hut = $scope.hut ;
+
+			return 'Huts filter: ' + input;
+		};
+	}
+]);
