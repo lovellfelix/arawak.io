58f8709ffea24a6004ceca5376aa9a173719578f
Lovell Felix <lovell.felix@gmail.com>
2015-07-05 17:10:41 -0500

    Added gravatar support

--- config/assets/production.js
+++ config/assets/production.js
@@ -16,7 +16,9 @@ module.exports = {
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-				'public/lib/angular-file-upload/angular-file-upload.min.js'
+				'public/lib/angular-file-upload/angular-file-upload.min.js',
+				'public/lib/angular-gravatar/build/angular-gravatar.min.js'
+
 			]
 		},
 		css: 'public/dist/application.min.css',
