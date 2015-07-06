9555faabf21b8779bb9be384d951ae8ebbe05ab0
Lovell Felix <lovell.felix@gmail.com>
2015-07-06 12:40:11 -0700

    Redirects to dashboard after login

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -13,7 +13,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				$scope.authentication.user = response;
 
 				// And redirect to the index page
-				$location.path('/huts/create');
+				$location.path('/dashboard');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
