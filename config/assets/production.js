3b43f4d806ecc09cba471ec69c78c6189326921f
Lovell Felix <lovell.felix@gmail.com>
2015-07-09 07:31:01 -0500

    Moved from angular material to material design lite

--- config/assets/production.js
+++ config/assets/production.js
@@ -9,14 +9,13 @@ module.exports = {
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
-				'public/lib/angular-aria/angular-aria.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
-				'public/lib/angular-material/angular-material.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
 				'public/lib/angular-file-upload/angular-file-upload.min.js',
+				'public/lib/material-design-lite/material.min.js',
 				'public/lib/angular-gravatar/build/angular-gravatar.min.js'
 
 			]
