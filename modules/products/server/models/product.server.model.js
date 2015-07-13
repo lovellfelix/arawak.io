6f4b33e2f75c34ecffb5b74e3ae410b90aa7ed11
Lovell Felix <lovell.felix@gmail.com>
2015-07-12 23:37:30 -0500

    Added products

---
+++ modules/products/server/models/product.server.model.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Product Schema
+ */
+var ProductSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Product name',
+		trim: true
+	},
+	version: {
+		type: String,
+		default: '',
+		required: 'Please fill Product version',
+		trim: true
+	},
+	description: {
+		type: String,
+		default: '',
+	},
+	repository: {
+		type: String,
+		default: '',
+		required: 'Please fill Product repository url',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Product', ProductSchema);
