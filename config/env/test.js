9e0eacd3e0696f0fa91808d5bfb78ef73e20b136
Lovell Felix <lovell.felix@gmail.com>
2015-07-14 07:20:26 -0500

    Added missing property for test

--- config/env/test.js
+++ config/env/test.js
@@ -40,5 +40,29 @@ module.exports = {
 				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
 			}
 		}
-	}
+	},
+	stripeOptions: {
+			apiKey: process.env.STRIPE_KEY || '',
+			stripePubKey: process.env.STRIPE_PUB_KEY || '',
+			defaultPlan: 'free',
+			plans: ['free', 'silver', 'gold', 'platinum'],
+			planData: {
+				'free': {
+					name: 'Free',
+					price: 0
+				},
+				'silver': {
+					name: 'Silver',
+					price: 9
+				},
+				'gold': {
+					name: 'Gold',
+					price: 19
+				},
+				'platinum': {
+					name: 'Platinum',
+					price: 29
+				}
+			}
+		}
 };
