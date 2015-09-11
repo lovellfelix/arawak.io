7163216bf853617c46a7212822eb21a8ba246530
Lovell Felix <lovell.felix@gmail.com>
2015-09-10 23:54:17 -0500

    Fixed routes

--- modules/huts/client/config/huts.client.routes.js
+++ modules/huts/client/config/huts.client.routes.js
@@ -12,19 +12,19 @@ angular.module('huts').config(['$stateProvider',
 		}).
 		state('huts.list', {
 			url: '',
-			templateUrl: 'modules/huts/views/list-huts.client.view.html'
+			templateUrl: 'modules/huts/client/views/list-huts.client.view.html'
 		}).
 		state('huts.create', {
 			url: '/create',
-			templateUrl: 'modules/huts/views/create-hut.client.view.html'
+			templateUrl: 'modules/huts/client/views/create-hut.client.view.html'
 		}).
 		state('huts.view', {
 			url: '/:hutId',
-			templateUrl: 'modules/huts/views/view-hut.client.view.html'
+			templateUrl: 'modules/huts/client/views/view-hut.client.view.html'
 		}).
 		state('huts.edit', {
 			url: '/:hutId/edit',
-			templateUrl: 'modules/huts/views/edit-hut.client.view.html'
+			templateUrl: 'modules/huts/client/views/edit-hut.client.view.html'
 		});
 	}
 ]);
